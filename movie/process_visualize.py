b13123b4df2cde19544db99bfd44349485115075
BoonDoPark <pbd0416@gmail.com>
2023-10-24 13:45:18 +0900

    movie file remove

--- movie/process_visualize.py
+++
@@ -1,8 +0,0 @@
-from visualize import DataVisualizer
-
-class MovieVisualize:
-    @staticmethod
-    def run(rate, xs: list, ys: list, file_name: str):
-        figure = DataVisualizer.visualize(rate, xs, ys)
-        DataVisualizer.save_to_img(file_name, figure)
-
